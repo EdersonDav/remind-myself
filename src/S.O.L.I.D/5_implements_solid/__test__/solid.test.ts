a7de35ee93d2da9ed72bbd292bbfe799b2a7d9b9
EdersonDavi <ederson.silva@sunoresearch.com.br>
2022-09-19 06:38:02 -0300

    feat: add foreach test

--- src/S.O.L.I.D/5_implements_solid/__test__/solid.test.ts
+++ src/S.O.L.I.D/5_implements_solid/__test__/solid.test.ts
@@ -2,38 +2,27 @@ import { IPayment } from '../IPayment';
 import { Payment } from '../Payment';
 import { factory } from '../factory';
 
+interface Tets{
+  class: string;
+  name: string;
+}
+
 describe('Payments Methods', ()=>{
+  describe.each([
+    {class: 'pagarme', name: 'Pagarme'},
+    {class: 'pagSeguro', name: 'Pague Seguro'},
+    {class: 'stripe', name: 'Stripe'},
+  ])('$name', (item: Tets) => {
+    it(`should payment with API`, ()=>{
+      const api:IPayment = factory(item.class);
+  
+      const pay = new Payment(api);
+  
+      const payment = pay.payCredit(2000)
+  
+      expect(payment).toBe(`${api.clientName} - payment with credit card value 2000`)
+  
+    })
+  });
 
-  it('should payment with Pagarme API', ()=>{
-    const apiPagarme:IPayment = factory('pagarme');
-
-    const pay = new Payment(apiPagarme);
-
-    const payment = pay.payCredit(2000)
-
-    expect(payment).toBe(`${apiPagarme.clientName} - payment with credit card value 2000`)
-
-  })
-
-  it('should payment with Pague Seguro API', ()=>{
-    const apipagSeguro:IPayment = factory('pagSeguro');
-
-    const pay = new Payment(apipagSeguro);
-
-    const payment = pay.payCredit(2000)
-
-    expect(payment).toBe(`${apipagSeguro.clientName} - payment with credit card value 2000`)
-
-  })
-
-  it('should payment with Stripe API', ()=>{
-    const apiStripe:IPayment = factory('stripe');
-
-    const pay = new Payment(apiStripe);
-
-    const payment = pay.payCredit(2000)
-
-    expect(payment).toBe(`${apiStripe.clientName} - payment with credit card value 2000`)
-
-  })
 })
--- src/S.O.L.I.D/5_implements_solid/factory.ts
+++ src/S.O.L.I.D/5_implements_solid/factory.ts
@@ -3,12 +3,18 @@ import { PagarMe } from './Clients/PagarMe'
 import { Stripe } from './Clients/Stripe'
 import { IPayment } from "./IPayment";
 
-export const factory = (method: 'stripe' | 'pagSeguro' | 'pagarme'): IPayment =>{
+interface IPaymentClients{
+  stripe: IPayment;
+  pagSeguro: IPayment;
+  pagarme: IPayment;
+}
+
+export const factory = (method: string): IPayment =>{
   const paymentClients = {
     stripe: Stripe,
     pagSeguro: PagSeguro,
     pagarme: PagarMe
   }
 
-  return new paymentClients[method]()
+  return new paymentClients[method as keyof IPaymentClients]()
 }
